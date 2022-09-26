20fec6812203e225a74532f9b2d16c0921085651
Enmanuel Sandoval <enmanuel.sandoval@globant.com>
2022-09-25 23:24:02 -0300

    Cambio de app.js text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> por su puesto que hay cambios aqui
+        </p>
+        <p>
+          lorem ipsum bla bla bla 
         </p>
         <a
           className="App-link"
