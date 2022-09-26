058d69114905985019c6f05c84787c45b726db37
Enmanuel Sandoval <enmanuel.sandoval@globant.com>
2022-09-26 00:00:55 -0300

    edicion de home page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> por su puesto que hay cambios aqui
-        </p>
-        <p>
-          lorem ipsum bla bla bla 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React. claro que si.
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return <h1>Hola Mundo</h1>
 }
 
-export default App;
+export default App
